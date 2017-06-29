c83fecab9577a79ae42c9e7b07d69a4b65496d41
unknown <cheetirala@gmail.com>
2017-06-29 21:09:14 +0100

    test

--- src/test/java/com/xe/tests/RunCukesTest.java
+++ src/test/java/com/xe/tests/RunCukesTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
  */
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", format = {"pretty", "json:target/cucumber.json"}, tags={"~@ignore"})
+@CucumberOptions(features = "src/test/resources/features/eurotopoundconversion.feature", format = {"pretty", "json:target/cucumber.json"}, tags={"~@ignore"})
 
 public class RunCukesTest {
 }
